e1b3c3fca10bf84b5742ff30c04f3a55ab15a917
Mac User <youcanreach_will@icloud.com>
2017-09-30 16:07:25 -0400

    UI changes contact, fixing array in payments

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -1,7 +1,9 @@
 class PurchasesController < ApplicationController
   def new
     @purchase = Purchase.new
-    @locations = ["Albright Court", "Albright Woods", "Crowell Hall", "Krause Hall", "Moan Hall", "North Hall", "Rockland Hall", "Smith Hall", "Walton Hall"]
+    @locations1 = ["Albright Court", "Albright Woods", "Crowell Hall", "Krause Hall", "Moan Hall", "North Hall", "Rockland Hall", "Smith Hall", "Walton Hall"]
+    @locations2 = ["Baxter Hall", "Coastal Towers Apartments", "Johnson Hall", "Oceanview Hall", "Weyenberg Hall", "Rinker Hall", "Flagler Towers Apartments", "Lakeview Apartments", "Samaritan Gardens Apartments", "Mango Apartments"]
+    @locations3 = ["Beck Hall", "Berks Hall", "Bonner Hall", "Deatrick Hall", "Dixon Hall", "Gold Bear Village South", "Gold Bear Village West", "Honors Building", "Johnson Hall", "Lehigh Hall", "Rothermel hall", "Schuyl Hall", "University Place"]
   end
 
   def create
@@ -19,7 +21,8 @@ class PurchasesController < ApplicationController
   private
 
     def purchases_params
-      params.require(:purchase).permit(:move_out_location, :move_in_location,
+      params.require(:purchase).permit(:move_out_location,
+                                        :move_in_location,
                                         :move_out_room,
                                         :move_in_room,
                                         :registration_fee_paid,
--- app/models/purchase.rb
+++ app/models/purchase.rb
@@ -1,8 +1,8 @@
 class Purchase < ApplicationRecord
   belongs_to :user
   validates_presence_of :move_out_location, :move_in_location,
-                        :move_out_room, :move_in_room, :move_out_date,
-                        :move_in_date, :amount
+                        :move_out_room, :move_in_room,
+                        :amount
   attr_accessor :ui_stripe_token
   # before_save :build_stripe_customer
 
